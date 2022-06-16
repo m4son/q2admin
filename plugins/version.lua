9ee90eedaa5fbacb4de7e4ad9ebfd5d606dbea88
m4son <mrsima@gmail.com>
2022-06-16 17:21:04 +0200

    version.lua  bugs

--- plugins/version.lua
+++ plugins/version.lua
@@ -5,6 +5,9 @@ simple !version* script by TgT
 		q2proded r548 or later
 		
 	Changelog:
+		v2.2
+		+ newline fix
+		+ dont crash if player is nil
 		v2.1
 		+ works with Aprq2 and Q2pro
 		v2.0
@@ -13,11 +16,11 @@ simple !version* script by TgT
 		+ make it universal for every client...
 --]]
 
-local version = "2.1"
+local version = "2.2"
 local word = "!versio.+"
 
 gi.AddCommandString("sets q2a_version "..version.."\n")
-gi.AddCommandString("addstuffcmd begin set version 0 u")
+gi.AddCommandString("addstuffcmd begin set version 0 u\n")
 
 function ClientCommand(client)
 	local sm = string.match
@@ -26,13 +29,17 @@ function ClientCommand(client)
 	match = sm(cmd,word)
 	
 	if match then
-		gi.cprintf(client,PRINT_HIGH,"num name            client version\n")
-		gi.cprintf(client,PRINT_HIGH,"--- --------------- ---------------------------------------------\n")
+		if ex.players[client] then
+			gi.cprintf(client,PRINT_HIGH,"num name            client version\n")
+			gi.cprintf(client,PRINT_HIGH,"--- --------------- ---------------------------------------------\n")
 		
-		for i,plr in pairs(ex.players) do
-			if plr.name ~= "[MVDSPEC]" then
-				gi.cprintf(client,PRINT_HIGH,"%3d %-15s %s\n", i-1,plr.name,plr.version)
+			for i,plr in pairs(ex.players) do
+				if plr.name ~= "[MVDSPEC]" then
+					gi.cprintf(client,PRINT_HIGH,"%3d %-15s %s\n", i-1,plr.name,plr.version)
+				end
 			end
+		else
+			gi.cprintf(client,PRINT_HIGH,"Could not run a command.\n")
 		end
 		return true
 	end
